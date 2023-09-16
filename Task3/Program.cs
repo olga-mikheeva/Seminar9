90f1d379d56a7e70965e849aa5952cebcc4168c5
Olga <workpost.mkh@yandex.ru>
2023-09-16 20:37:41 +0300

    Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int ReturnSumNumberHimDigits(int number)
+{
+    if(number == 0) return 0;
+    return ReturnSumNumberHimDigits(number / 10) + (number % 10);    
+}
+
+int number = InputNum("Введите число: ");
+int sum = ReturnSumNumberHimDigits(number);
+Console.WriteLine(sum);
