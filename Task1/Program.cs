194e4b1825e58de41ede71d4e0d9b01377996c26
Olga <workpost.mkh@yandex.ru>
2023-09-16 15:59:55 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N = 6 -> "1, 2, 3, 4, 5, 6"
+*/
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintNumbers(int n, int count) // в void можем прописать return без значения
+{
+    if (count < n)
+    {
+        count++;
+        Console.Write(count+ " ");
+        PrintNumbers(n, count); // и снова вызывается функция, внутри т.к. вызов функции должен быть под условием
+    }
+    
+}
+
+int number = InputNum("Введите число: ");
+PrintNumbers(number, 0);
+
